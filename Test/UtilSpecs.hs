a37e33824f70bd007f057400fd3b3a56c9504252
Mike Burns <mike@mike-burns.com>
2014-04-01 15:11:44 +0200

    Check for afterElem
    Selecting the element after the first element gives either `Nothing` or
    `Just`, based on whether there is such an element.

--- Test/UtilSpecs.hs
+++ Test/UtilSpecs.hs
@@ -9,10 +9,20 @@ import Rcm.Util (at, afterElem)
 utilSpecs = describe "Rcm.Util" $ do
   context "at" $ do
     it "produces missing and existing values as correct" $ property $
-      prop_at_missing_existing
+      prop_atMissingExisting
 
-prop_at_missing_existing :: Positive Int -> [Char] -> Bool
-prop_at_missing_existing pIdx xs =
+  context "afterElem" $ do
+    it "produces missing and existing values as correct" $ property $
+      prop_afterElemMissingExisting
+
+prop_atMissingExisting :: Positive Int -> [Char] -> Bool
+prop_atMissingExisting pIdx xs =
   let idx = getPositive pIdx
       assertion = if length xs > idx then isJust else isNothing in
   assertion $ xs `at` idx
+
+prop_afterElemMissingExisting :: [Char] -> Bool
+prop_afterElemMissingExisting [] = isNothing $ 'x' `afterElem` []
+prop_afterElemMissingExisting xss@(x:xs) =
+  let assertion = if null xs then isNothing else isJust in
+  assertion $ x `afterElem` xss
